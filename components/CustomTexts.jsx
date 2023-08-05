110ea49301469933860f769d1eda963ae45517aa
Akinwumidi <israelakin14@gmail.com>
2023-08-05 22:41:00 +0100

    complete anout section for desktop and smaller screens

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,23 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
+
+export const TypingText = ({ tittle, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    initial="hidden"
+    whileInView="show"
+    className={`${textStyles} font-extrabold text-[36px] text-secondary-white`}
+  >
+    {Array.from(tittle).map((letter, index) => (
+      <motion.span key={index} variants={textVariant2}>
+        {letter === " " ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
+export const TitleText = ({ tittle, textStyles }) => (
   <h2>Title Text</h2>
 );
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => (
       <div className="flex items-center flex-col justify-center relative z-10">
         <motion.h1
           variants={textVariant(1.1)}
-          intial="hidden"
+          initial="hidden"
           whileInView="show"
           className={styles.heroHeading}
         >
@@ -37,8 +37,8 @@ const Hero = () => (
       <motion.div
         variants={slideIn('right', "tween", 0.2, 1)}
         className='relative w-full md:-mt-[20px] -mt-[12px]'
-        initial="hidden"
-        whileInView="show"
+      // initial="hidden"
+      // whileInView="show"
       >
         <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
         <img src="/cover.png" alt="cover" className='w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative' />
