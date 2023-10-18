f66aad1fbc284286a69c84d57561c578d07f67d6
Akinwumidi <israelakin14@gmail.com>
2023-10-18 13:45:01 +0100

    complete world section for small and larger screens

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,30 @@
 'use client';
+import styles from '../styles';
+import { motion } from 'framer-motion';
+import { TypingText, NewFeatures, TitleText } from '../components';
+import { staggerContainer, fadeIn } from '../utils/motion';
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{ once: 'false', amount: 0.25 }} className={`${styles.innerWidth} mx-auto flex flex-col`}>
+      <TypingText tittle="| People on the World" textStyles="text-center" />
+
+      <TitleText tittle={<>Track friends around you and invite them to play together in the same world</>} textStyles="text-center" />
+
+      <motion.div variants={fadeIn('up', 'tween', 0.3, 1)} className='realtive mt-[68px] flex w-full h-[550px]'>
+        <img src="/map.png" alt="map" className=' w-full h-full object-cover' />
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img alt="people" src="people-01.png" className='w-full h-full' />
+        </div>
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img alt="people" src="people-02.png" className='w-full h-full' />
+        </div>
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img alt="people" src="people-03.png" className='w-full h-full' />
+        </div>
+      </motion.div>
+
+    </motion.div>
   </section>
 );
 
